845309d340d8fe2b3a40ea85aed8a3c33a275f1e
Svetlana <isaeva322@mail.ru>
2022-11-16 22:39:47 +0300

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,40 @@ public class Main {
       //  Задание 1
         System.out.println(" Задание 1 ");
 
-       int clientOS = 0;
+       int clientOS = 1;
        if (clientOS == 0 ) {
            System.out.println("Установите версию приложения для iOS по ссылке");
        }else if ( clientOS == 1) {
                System.out.println( " Установите версию приложения для Android по ссылке ");
            }
+
+
+        //  Задание 2
+        System.out.println(" Задание 2 ");
+        int clientDeviceYear = 2009;
+        if ( clientOS == 0 && clientDeviceYear >= 2015 ){
+            System.out.println( " Установите обычную версию приложения для iOS по ссылке");
+        } else if ( clientOS == 0 && clientDeviceYear < 2015 ){
+            System.out.println( " Установите облегченную версию приложения для iOS по ссылке");
+        }
+        if ( clientOS == 1 && clientDeviceYear >= 2015 ){
+            System.out.println( " Установите обычную версию приложения для Android по ссылке");
+        } else if ( clientOS == 1 && clientDeviceYear < 2015 ){
+            System.out.println( " Установите облегченную версию приложения для Android по ссылке");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     }
